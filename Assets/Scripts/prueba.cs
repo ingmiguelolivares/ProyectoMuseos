7ce50dcb804a990e9d0ad45a16df83c2c76f2ede
ingmiguelolivares <miguelolivares1981@gmail.com>
2025-10-16 10:17:35 -0500

    prueba

---
+++ Assets/Scripts/prueba.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class prueba : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    //codigo de prueba
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
